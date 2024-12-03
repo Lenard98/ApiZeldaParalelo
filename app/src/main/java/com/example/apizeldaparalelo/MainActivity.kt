97ea513f8372f6a911f09d8eab136d7e48a455a0
libnyvr <163462086+libnyvr@users.noreply.github.com>
2024-12-03 17:27:42 -0600

    imagecard

--- app/src/main/java/com/example/apizeldaparalelo/MainActivity.kt
+++ app/src/main/java/com/example/apizeldaparalelo/MainActivity.kt
@@ -3,13 +3,27 @@ package com.example.apizeldaparalelo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.apizeldaparalelo.ui.theme.ApiZeldaParaleloTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,13 +33,67 @@ class MainActivity : ComponentActivity() {
             ApiZeldaParaleloTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Modifier .fillMaxSize()
+                    MaterialTheme.colorScheme.background
+                    Local()
                 }
             }
         }
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun Local() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            item {
+                ElevatedCard(
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color.Black
+                    ),
+                    modifier = Modifier.size(width = 700.dp, height = 500.dp)
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = "Nirvana: IN UTERO",
+                            modifier = Modifier.padding(16.dp),
+                            color = Color.White,
+                            textAlign = TextAlign.Center
+                        )
+
+                        Image(
+                            painter = painterResource(R.drawable.ic_launcher_background),
+                            contentDescription = "xd1",
+
+                            )
+
+                        Text(
+                            text = "In Utero es el tercer y último álbum de estudio de la banda estadounidense de grunge Nirvana, lanzado en septiembre de 1993 por DGC Records.",
+                            modifier = Modifier.padding(16.dp),
+                            color = Color.White,
+                            textAlign = TextAlign.Left
+                        )
+                    }
+                }
+            }
+
+
+
+
+        }
+    }
+}
